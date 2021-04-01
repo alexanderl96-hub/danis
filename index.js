733bfda7563b17ed8a6a5598d1a57254972781ae
Alexander <alexanderl96@yahoo.com>
2021-04-01 15:12:33 -0400

    first page pro

---
+++ index.js
@@ -0,0 +1,16 @@
+const imagen = document.getElementById('home')
+const header = document.querySelector('header')
+const p2 = document.querySelector('p')
+document.addEventListener('DOMContentLoaded', e =>{
+    e.preventDefault()
+
+    // imagen.addEventListener('click', e =>{
+    //     e.preventDefault()
+    //     if(e.target){
+    //         p2.textContent = "Galery"
+    //         header.appendChild(p2)
+    //     }
+    
+    // })
+})
+
